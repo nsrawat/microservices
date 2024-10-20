4ea24767e5e4ae34b3feb55048fbf00d04545280
nsrawat <nsrawat89623@gmail.com>
2024-10-20 15:31:17 +0530

    AuditAwareImpl added

--- section-9/accounts/src/main/java/com/microservices/accounts/audit/AuditAwareImpl.java
+++ section-9/accounts/src/main/java/com/microservices/accounts/audit/AuditAwareImpl.java
@@ -1,4 +1,4 @@
-package com.eazybytes.loans.audit;
+package com.microservices.accounts.audit;
 
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.stereotype.Component;
--- section-9/cards/src/main/java/com/eazybytes/cards/audit/AuditAwareImpl.java
+++ section-9/cards/src/main/java/com/eazybytes/cards/audit/AuditAwareImpl.java
@@ -1,4 +1,4 @@
-package com.eazybytes.loans.audit;
+package com.eazybytes.cards.audit;
 
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.stereotype.Component;
